fac5b95b63c559e309a16c4eae3daf87c13de7b6
María <mariagomezmateo97@gmail.com>
2024-03-13 19:59:09 +0100

    commit inicial

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -4,7 +4,7 @@ export const createFooter = () => {
   const myFooter = document.querySelector('footer')
   myFooter.innerHTML = `
     <a href="https://www.linkedin.com/in/mar%C3%ADa-g%C3%B3mez-mateo-941071171/" target="_blank">
-      <img src="/public/assets/linkedin.png" alt="LinkedIn">
+      <img src="/assets/linkedin.png" alt="LinkedIn">
     </a>
     <a href="mailto:mariagomezmateo97@gmail.com">mariagomezmateo97@gmail.com</a>
     <a href="tel:+34600000000">+34 600 000 00</a>
--- src/Components/Main/AboutMe/aboutme.js
+++ src/Components/Main/AboutMe/aboutme.js
@@ -5,7 +5,7 @@ export const aboutMe = () => {
   const divAboutMe = document.createElement('div')
   divAboutMe.classList.add('aboutMe')
   divAboutMe.innerHTML = `
-  <img src="/public/assets/IMG_4925.jpg"" alt="María Gómez">
+  <img src="/assets/IMG_4925.jpg"" alt="María Gómez">
   <h1>${data.name}</h1>
   <h2>${data.email}</h2>
   <p>${data.aboutMe}</p>
